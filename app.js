bfa1f55e9d2eba357088c3ffa50ad877381eeb8b
Ali Jane <alison.smith@dowjones.com>
2018-06-22 19:58:56 -0400

    working jquery

--- app.js
+++ app.js
@@ -5,10 +5,54 @@ $(document).ready(function(){
     $("button.WRONG").click(function(){
         alert("better luck next time!");
     });
-    $("button").click(function(){
+    $("button.WRONG").click(function(){
         $("#qBox1")
         .slideUp(2000);
     });
-//next create a function that adds ++ to tally score //
+});
+var h1 = document.getElementsByTagName('h1')[0],
+  start = document.getElementById('start'),
+  stop = document.getElementById('stop'),
+  clear = document.getElementById('clear'),
+  seconds = 0,
+  minutes = 0,
+  hours = 0,
+  t;
 
-})
+function add() {
+  seconds++;
+  if (seconds >= 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes >= 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+  timer();
+}
+
+function timer() {
+  t = setTimeout(add, 1000);
+}
+timer();
+
+
+/* Start button */
+start.onclick = timer;
+
+/* Stop button */
+stop.onclick = function() {
+  clearTimeout(t);
+}
+
+/* Clear button */
+clear.onclick = function() {
+  h1.textContent = "00:00:00";
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+}
